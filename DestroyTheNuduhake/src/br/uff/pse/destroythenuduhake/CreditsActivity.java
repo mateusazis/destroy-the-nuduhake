e0a9ffcace00888e8b366b49b03078ac90b8ee95
loxorolim <loxorolim@gmail.com>
2013-11-20 21:01:32 -0200

    Criada nova imagem de creditos

---
+++ DestroyTheNuduhake/src/br/uff/pse/destroythenuduhake/CreditsActivity.java
@@ -0,0 +1,25 @@
+package br.uff.pse.destroythenuduhake;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class CreditsActivity extends Activity{
+
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.credits);
+
+		
+		
+		
+	}
+	
+
+
+}
