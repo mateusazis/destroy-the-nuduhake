4ecdf264cd582cb216ec2bb9b6dab8f9d60bf089
loxorolim <loxorolim@gmail.com>
2013-11-27 19:17:26 -0200

    Removido umas coisas inúteis.

--- DestroyTheNuduhake/src/br/uff/pse/destroythenuduhake/dtn/BundleReceiver.java
+++
@@ -1,8 +0,0 @@
-package br.uff.pse.destroythenuduhake.dtn;
-
-import br.uff.pse.destroythenuduhake.game.control.AssetBundle;
-
-public interface BundleReceiver
-{
-	public void onReceive(AssetBundle[] bundles);
-}
--- DestroyTheNuduhake/src/br/uff/pse/destroythenuduhake/dtn/ShareService.java
+++
@@ -1,9 +0,0 @@
-package br.uff.pse.destroythenuduhake.dtn;
-
-import br.uff.pse.destroythenuduhake.game.control.AssetBundle;
-
-public interface ShareService 
-{
-	public void start(BundleReceiver receiver, AssetBundle[] bundles);
-
-}
